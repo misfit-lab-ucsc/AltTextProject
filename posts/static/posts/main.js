716b5cfce28546747c40d57c68f6ec04067cf21d
Aaron Dangc <adraging@gmail.com>
2023-04-24 22:39:01 -0700

    added dark mode

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,19 +2,30 @@ console.log('Hello World')
 
 // toggling the theme for our page
 const toggleButton = document.getElementById("theme-toggle");
-const body = document.querySelector("body");
+const body = document.body;
+// document.querySelector("body");
 
 toggleButton.addEventListener("click", () => {
-    console.log("clicked");
+
     body.classList.toggle("dark-theme");
+    toggleButton.textContent = body.classList.contains("dark-theme") ? "Light Mode" : "Dark Mode";
+  
+    toggleButton.classList.toggle('light-button', !body.classList.contains("dark-theme"));
+    console.log('toggle ')
 
+
+
+    // console.log("clicked");
+    // body.classList.toggle("dark-theme");
     // get current text content of the button
-    const currentText = toggleButton.textContent;
-
+    // const currentText = toggleButton.textContent;
     // update text based on current state of body class
-    if (body.classList.contains("dark-theme")) {
-        toggleButton.textContent = "Light Mode";
-    } else {
-        toggleButton.textContent = "Dark Mode";
-    }
+    // if (body.classList.contains("dark-theme")) {
+    //     console.log('light style')
+    //     toggleButton.textContent = "Light Mode";
+    // } else {
+    //     console.log('dark style')
+    //     toggleButton.textContent = "Dark Mode";
+    //     toggleButton.classList.toggle('light-button');
+    // }
 });
