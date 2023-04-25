031c656e628a10a1a3d2559d2bc24069e1633097
Aaron Dangc <adraging@gmail.com>
2023-04-25 14:24:42 -0700

    cleaning up code

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -3,29 +3,11 @@ console.log('Hello World')
 // toggling the theme for our page
 const toggleButton = document.getElementById("theme-toggle");
 const body = document.body;
-// document.querySelector("body");
 
 toggleButton.addEventListener("click", () => {
-
     body.classList.toggle("dark-theme");
     toggleButton.textContent = body.classList.contains("dark-theme") ? "Light Mode" : "Dark Mode";
-  
     toggleButton.classList.toggle('light-button', !body.classList.contains("dark-theme"));
     console.log('toggle ')
-
-
-
-    // console.log("clicked");
-    // body.classList.toggle("dark-theme");
-    // get current text content of the button
-    // const currentText = toggleButton.textContent;
-    // update text based on current state of body class
-    // if (body.classList.contains("dark-theme")) {
-    //     console.log('light style')
-    //     toggleButton.textContent = "Light Mode";
-    // } else {
-    //     console.log('dark style')
-    //     toggleButton.textContent = "Dark Mode";
-    //     toggleButton.classList.toggle('light-button');
-    // }
+    // need to implement styles change for navbar as well and other classes
 });
