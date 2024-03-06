4415bc674dd82ef122ac095cdca8d2445c668107
Inan Xu <ixu2002new@gmail.com>
2024-03-05 22:39:17 -0800

    add support for AWS DB

--- posts/admin.py
+++ posts/admin.py
@@ -4,5 +4,5 @@ from .models import Post
 # Register your models here.
 class imageAdmin(admin.ModelAdmin):
     # tells django admin to display contents in dashboard
-    list_display = ['title','photo','alt_text','date_posted','author','last_updated_by']
+    list_display = ['id','title','photo','alt_text','date_posted','author','last_updated_by']
 admin.site.register(Post,imageAdmin)
--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     image = models.ImageField(default='default_profile_pic.jpg',upload_to='profile_pics')
+    desc = models.TextField(max_length=150, default='')
 
     def __str__(self):
         return f'{self.user.username} Profile'
