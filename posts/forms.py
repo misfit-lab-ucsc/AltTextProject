ba657d017b52826addcfc8079db44099ac46990d
Inan Xu <ixu2002new@gmail.com>
2024-05-29 11:32:41 -0700

    add suggestions to post forms

--- posts/forms.py
+++ posts/forms.py
@@ -12,9 +12,13 @@ class PostForm(forms.ModelForm):
         fields = ['photo','alt_text','tags']
     
     photo = forms.ImageField(required=True, label='Photo (2MB max):')
+    
     alt_text = forms.CharField(required=True,
                                label='Alt Text:',
                                widget=forms.TextInput(attrs={'placeholder': 'Please accurately describe the image!'}))
+    suggested_alt_text = ""
+
     tags = forms.CharField(required=True,
                            label='Tags (separate tags by comma):',
                            widget=forms.TextInput(attrs={'placeholder': 'Tag1, Tag2, Tag3, ...'}))
+    suggested_tags = []
